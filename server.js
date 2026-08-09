10e9e47e18090f0c2d7597c1a014613b37ff7cc3
riswandaP <riswandaputra03@gmail.com>
2026-08-09 19:22:59 +0700

    Sprint1-add reusable navbar & footer partials

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const path = require("path");
 const express = require("express");
+const pageRoutes = require("./routes/pages");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// View Engine
+// View engine EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Static Files
+// Static assets
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route sementara
-app.get("/", (req, res) => {
-    res.render("index");
-});
+// Routing halaman
+app.use("/", pageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
